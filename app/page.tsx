b839d0f4762013893c64fb7485cf4df3cf173244
HassaanDeveloper <mohammedhassaan449@gmail.com>
2024-11-09 17:27:33 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,46 @@
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+      <header className="bg-[#A29875] h-auto w-full flex flex-col md:flex-row items-center justify-between p-4 md:p-6 lg:px-14">
+        <h1 className="text-[#FFFFFF] w-[439px] h-[90px] flex items-center justify-center md:justify-start font-normal text-7xl mb-4 md:mb-0">
+          Hassaan
+        </h1>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <section className='flex flex-col md:flex-col sm:flex-col lg:flex-row'>
+
+        <div className="w-full md:w-4/5 sm-center:w-[100%]   lg:w-[89%] xl:w-[90%] flex flex-col items-center justify-center h-screen px-4 md:px-8 lg:px-8 space-y-6 md:space-y-8 lg:space-y-10">
+          <h2 className="text-[#000000] w-[496px] h-[189px] text-3xl sm:text-4xl md:text-5xl sm-center:text-center lg-center:text-left  lap:text-47px font-bold text-center md:text-left leading-tight md:leading-snug">
+            IMPECCABLE CRAFTSMANSHIP AND FINESSE
+          </h2>
+
+          <p className="w-full sm:w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12 text-[#787054] font-medium text-sm sm:text-base md:text-lg lg:text-xl text-center md:text-left leading-relaxed lg:-ml-[100px]">
+            An example of intricate workmanship and detail, elegant necklaces and long and short chains form a part of our desirable collection.
+          </p>
+
+          <button className="text-2xl bg-[#A29875] w-64 h-14 text-white rounded-lg p-2">
+            Explore Now
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        
+        <div className="relative w-full flex justify-center items-center">
+          <div className='relative  max-w-xs md:max-w-sm lg:max-w-md xl:max-w-lg font-medium text-xl flex justify-center'>
+
+            <Image
+              src="/Images/image.png"
+              alt="Image"
+              width={300}
+              height={400}
+              className="w-[360px] h-auto object-contain rounded-tl-[150px] rounded-br-[150px] shadow-lg hover:shadow-2xl hover:opacity-75"
+            />
+          </div>
+        </div>
+      </section>
+    </>
   );
-}
+};
+
+export default Home;
